6e4d0147ba13ba9e19897b6e32db9c45da090fbd
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-10-21 14:01:56 -0300

    mais um teste e action show consumindo a api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'warehouses#index'
+
+  resources :warehouses, only: [:show]
 end
--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -21,4 +21,39 @@ describe "Usuário visita tela inicial" do
 
     expect(page).to have_content 'Nenhum galpão encontrado'
   end
+
+  it 'e vê detalhes de galpão' do
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/warehouses/5").and_return(fake_response)
+
+    visit root_path
+    click_on 'Aeroporto SP'
+
+    expect(page).to have_content 'Galpão GRU - Aeroporto SP'
+    expect(page).to have_content 'Guarulhos'
+    expect(page).to have_content 'Avenida do Aeroporto, 1000 - CEP 15000-000'
+    expect(page).to have_content '100000 m2'
+    expect(page).to have_content 'Galpão destinado para cargas internacionais'
+  end
+  it 'e não é possivel carregar o galpão' do
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+
+    error_response = double("faraday_response", status: 500, body: "{}")
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/warehouses/5").and_return(error_response)
+
+    visit root_path
+    click_on 'Aeroporto SP'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Não foi possível carregar o galpão'
+
+  end
+  
 end
